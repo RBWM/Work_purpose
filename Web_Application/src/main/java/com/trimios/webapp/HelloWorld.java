e8b3d8a226680476d413eabc25439f6e7b222029
venky504504 <venky504504@gmail.com>
2018-08-29 21:42:22 +0530

    Update HelloWorld.java

--- Web_Application/src/main/java/com/trimios/webapp/HelloWorld.java
+++ Web_Application/src/main/java/com/trimios/webapp/HelloWorld.java
@@ -3,12 +3,7 @@ package com.trimios.webapp;
 
 public class HelloWorld {
    public static void main(String[] args) {
-      
-      //sample
       System.out.println("Hello, World"); 
-       System.out.println("Hello, World"); 
-       System.out.println("Hello, World"); 
-       System.out.println("Hello, World"); 
-       System.out.println("Hello, World"); 
+      
    }
 }
