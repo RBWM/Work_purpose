0e33529ef22c754ecd5bfbd54b8f5c86cb05f980
venky504504 <venky504504@gmail.com>
2018-09-24 22:42:26 +0530

    Update HelloWorld.java

--- Web_Application/src/main/java/com/trimios/webapp/HelloWorld.java
+++ Web_Application/src/main/java/com/trimios/webapp/HelloWorld.java
@@ -6,10 +6,7 @@ public class HelloWorld {
       
 
       System.out.println("Hello, World"); 
-      System.out.println("Hello, World");
-      System.out.println("Hello, World");
-      System.out.println("Hello, World");
-      System.out.println("Hello, World");
+      
       
        
       
